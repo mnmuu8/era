be85ef81d3de5ad2d1aacbfb93271921c5df2341
mnmuu8 <mnm.uu8@gmail.com>
2022-12-20 01:01:49 +0900

    create a selector

--- src/templates/SignIn.jsx
+++ src/templates/SignIn.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { push } from 'connected-react-router';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserId } from '../reducks/users/selectors';
 
 const SignIn = () => {
   const dispatch = useDispatch();
+  const selector = useSelector(state => state)
+  const uid = getUserId(selector)
   return (
     <div>
       <h2>サインイン</h2>
+      <p>{uid}</p>
       <button onClick={() => dispatch(push("/"))}>HOMEへ戻る</button>
     </div>
   )
