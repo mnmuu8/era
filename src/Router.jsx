b69612353a19fee97b7c90052947337f055093cb
mnmuu8 <mnm.uu8@gmail.com>
2022-12-20 11:21:09 +0900

    firebase settings and crate sign-up process

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { SignIn, Home } from "./templates";
+import { SignIn, SignUp, Home } from "./templates";
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path={"/"} component={Home} />
       <Route exact path={"/signin"} component={SignIn} />
+      <Route exact path={"/signup"} component={SignUp} />
     </Switch>
   )
 }
---
+++ src/templates/SignUp.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { TextInput, PrimaryButton } from '../components/UIkit'
+import { signUp } from '../reducks/users/operations'
+
+const SignUp = () => {
+
+  const dispatch = useDispatch();
+
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPasswrod, setConfirmPassword] = useState("")
+
+  const inputUsername = useCallback((e) => {
+    setUsername(e.target.value)
+  }, [setUsername]) 
+  const inputEmail = useCallback((e) => {
+    setEmail(e.target.value)
+  }, [setEmail]) 
+  const inputPassword = useCallback((e) => {
+    setPassword(e.target.value)
+  }, [setPassword]) 
+  const inputConfirmPassword = useCallback((e) => {
+    setConfirmPassword(e.target.value)
+  }, [setConfirmPassword]) 
+  
+  return (
+    <div>
+      <h2>アカウント登録</h2>
+      <TextInput 
+        fullWidth={true} label={"ユーザー名"} multiline={false} required={true}
+        rows={1} value={username} type={"text"} onChange={inputUsername}
+      />
+      <TextInput 
+        fullWidth={true} label={"メールアドレス"} multiline={false} required={true}
+        rows={1} value={email} type={"email"} onChange={inputEmail}
+      />
+      <TextInput 
+        fullWidth={true} label={"パスワード"} multiline={false} required={true}
+        rows={1} value={password} type={"password"} onChange={inputPassword}
+      />
+      <TextInput 
+        fullWidth={true} label={"パスワード（確認用）"} multiline={false} required={true}
+        rows={1} value={confirmPasswrod} type={"password"} onChange={inputConfirmPassword}
+      />
+      <PrimaryButton label={"アカウントを登録する"} onClick={() => dispatch(signUp(username, email, password, confirmPasswrod))} />
+    </div>
+  )
+}
+
+export default SignUp
