86dab50e38c8582d978b79c733bf2ac3d3ab33a2
mnmuu8 <mnm.uu8@gmail.com>
2022-12-20 09:31:04 +0900

    store and operations settings

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,6 +1,7 @@
 import { createStore as reduxCreateStore, combineReducers, applyMiddleware } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import { UsersReducer } from "../users/reducers";
+import thunk from "redux-thunk";
 
 export default function createStore(history) {
   return reduxCreateStore(
@@ -9,7 +10,8 @@ export default function createStore(history) {
       users: UsersReducer
     }),
     applyMiddleware(
-      routerMiddleware(history)
+      routerMiddleware(history),
+      thunk
     )
   )
 }
