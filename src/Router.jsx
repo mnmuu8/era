4fc162b0a2b7f54d2a77ac20a29aa031ee436ffb
mnmuu8 <mnm.uu8@gmail.com>
2022-12-20 21:48:18 +0900

    create product detail and set up swiper

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import Auth from "./Auth";
 import { SignIn, Reset, SignUp, ProductEdit, ProductList } from "./templates";
+import ProductDetail from "./templates/ProductDetail";
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
       
       <Auth>
         <Route exact path={"(/)?"} component={ProductList} />
+        <Route exact path={"/product/:id"} component={ProductDetail} />
         <Route path={"/product/edit(/:id)?"} component={ProductEdit} />
       </Auth>
     </Switch>
---
+++ src/templates/ProductDetail.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { db } from '../firebase';
+import HTMLReactParser from "html-react-parser"
+import { ImageSwiper } from '../components/products';
+
+const returnCodeToBr = (text) => {
+  if (text === "") {
+    return text
+  } else {
+    return HTMLReactParser(text.replace(/\r?\n/g, '<br/>'))
+  }
+}  
+
+const ProductDetail = () => {
+  const selector = useSelector(state => state);
+  const path = selector.router.location.pathname;
+  const id = path.split("/product/")[1]
+
+  const [product, setProduct] = useState(null)
+
+  useEffect(() => {
+    db.collection("products").doc(id).get()
+      .then(doc => {
+        const data = doc.data();
+        setProduct(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      {product && (
+        <div>
+          <ImageSwiper images={product.images} />
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+          <p>{product.quantity}</p>
+          <p>{returnCodeToBr(product.description)}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProductDetail
