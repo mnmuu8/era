2b654dc7bed873d7f19ca76609b5e2be155438fc
mnmuu8 <mnm.uu8@gmail.com>
2022-12-21 10:48:49 +0900

    create drawer menus

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getIsSignedIn } from '../../reducks/users/selectors'
 import HeaderMenus from './HeaderMenus'
 import { push } from 'connected-react-router'
+import ClosableDrawer from './ClosableDrawer'
 
 const Header = () => {
   const selector = useSelector(state => state)
   const dispatch = useDispatch()
   const isSignedIn = getIsSignedIn(selector)
+
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerToggle = useCallback((e) => {
+    if (e.type === "keydown" && (e.key === "Tab" || e.key === "Shift")) {
+      return
+    }
+    setOpen(!open)
+  }, [setOpen, open])
   
   return (
-    <header className='c-header'>
-      <div className='inner'>
-        <h1 onClick={() => dispatch(push("/"))}>ロゴ</h1>
-        {isSignedIn && (
-          <div>
-            <HeaderMenus />
-          </div>
-        )}
-      </div>
-    </header>
+    <>
+      <header className='c-header'>
+        <div className='inner'>
+          <h1 onClick={() => dispatch(push("/"))}>ロゴ</h1>
+          {isSignedIn && (
+            <div>
+              <HeaderMenus handleDrawerToggle={handleDrawerToggle} />
+            </div>
+          )}
+        </div>
+      </header>
+      <ClosableDrawer open={open} onClose={handleDrawerToggle} />
+    </>
   )
 }
 
--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -5,7 +5,7 @@ import ShoppingCart from '@material-ui/icons/ShoppingCart'
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder'
 import MenuIcon from '@material-ui/icons/Menu';
 
-const HeaderMenus = () => {
+const HeaderMenus = (props) => {
   return (
     <>
       <IconButton>
@@ -16,7 +16,7 @@ const HeaderMenus = () => {
       <IconButton>
         <FavoriteBorder />
       </IconButton>
-      <IconButton>
+      <IconButton onClick={(e) => props.handleDrawerToggle(e)}>
         <MenuIcon />
       </IconButton>
     </>
